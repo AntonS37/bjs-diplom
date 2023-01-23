ced497c4d75c488727c52fb5b810657949f1595f
Neto <5308014@mail.ru>
2023-01-23 13:13:23 +0300

    the diploma is ready

---
+++ public/loginPage.js
@@ -0,0 +1,16 @@
+"use strict"
+
+const userForm = new UserForm();
+userForm.loginFormCallback = data => ApiConnector.login(data, response => {
+    if (!response.success) {
+        return userForm.setLoginErrorMessage(response.error);
+    }
+    location.reload();
+});
+
+userForm.registerFormCallback = newData => ApiConnector.register(newData, response => {
+    if (!response.success) {
+        return userForm.setRegisterErrorMessage(response.error);
+    }
+    location.reload();
+});
